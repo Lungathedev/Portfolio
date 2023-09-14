45a05c658a915a088ad814d4bddc31205ed82142
Lungathedev <144711874+Lungathedev@users.noreply.github.com>
2023-09-14 14:06:58 +0200

    Update Home.php

--- Home.php
+++ Home.php
@@ -86,11 +86,12 @@
 							<h2 class="font-secondary">My side of the story</h2>
 						</div>
 						<div style="margin: auto; display: block;">
-									<video poster="Rocket-launch.jpeg" style="height: auto; width: 100%;" controls>
+								<!--	<video poster="Rocket-launch.jpeg" style="height: auto; width: 100%;" controls> -->
 									<!-- Video-Source-URL -->
   									<source src="https://drive.google.com/file/d/1oKkZPZqV0iZOQlUWNVU1dgcOJGYWM4Pi/view?usp=sharing" type="video/mp4">
  									Your browser does not support the video tag.
 									</video>
+						<iframe src="https://drive.google.com/file/d/1oKkZPZqV0iZOQlUWNVU1dgcOJGYWM4Pi/preview" style="height: auto; width: 100%;" allow="autoplay"></iframe>
 						</div>
 						<div class="row justify-content-center col-mb-50 mb-0"></div>
 					</div>
