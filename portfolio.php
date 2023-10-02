d2a2f874cfc9ab92c607bcd0057d30f9df52d814
Lunga Magagula <144711874+Lungathedev@users.noreply.github.com>
2023-10-02 16:26:42 +0200

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -112,7 +112,8 @@
 									<!-- About-Me-Section-Header -->
 									<h2 class="font-secondary">Tech stack</h2>
 									<!-- About-Me-Section-Body-Text -->
-									<img title="PHP" alt="PHP" src="php.png" width="30px">
+									<img title="PHP" alt="PHP" src="php.png" width="60px">
+									<img title="HTML/JavaScript/CSS" alt="HTML/JavaScript/CSS" src="html.png" width="60px">
 									</div>
 								<table class="table">
 									<tbody>
